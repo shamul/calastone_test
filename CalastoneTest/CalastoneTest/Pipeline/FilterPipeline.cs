6badc06ce6db68881a2bcdd4d81331d603472181
shamul <shamul@evo-is.com>
2022-06-19 21:03:42 +0100

    added pipeline

---
+++ CalastoneTest/CalastoneTest/Pipeline/FilterPipeline.cs
@@ -0,0 +1,22 @@
+﻿using CalastoneTest.Filter;
+
+namespace CalastoneTest.Pipeline
+{
+    public class FilterPipeline
+    {
+        private readonly IList<IFilter> _filters = new List<IFilter>();
+
+        public void AddFilter(IFilter filter)
+        {
+            _filters.Add(filter);
+        }
+
+        public IEnumerable<string> Process(IEnumerable<string> input)
+        {
+            foreach (var filter in _filters)
+                input = filter.Execute(input);
+
+            return input;
+        }
+    }
+}
---
+++ CalastoneTest/CalastoneUnitTests/Pipeline/FilterPipelineTests.cs
@@ -0,0 +1,38 @@
+﻿using AutoFixture;
+using AutoFixture.AutoMoq;
+using AutoFixture.NUnit3;
+using CalastoneTest.Filter;
+using CalastoneTest.Pipeline;
+using Moq;
+using NUnit.Framework;
+
+namespace CalastoneUnitTests.Pipeline
+{
+    [TestFixture]
+    public class FilterPipelineTests
+    {
+        private readonly IFixture _fixture = new Fixture();
+
+        [SetUp]
+        public void Setup()
+        {
+            _fixture.Customize(new AutoMoqCustomization());
+        }
+
+        [Test]
+        [AutoData]
+        public void ProcessFiltersInPipeline(IList<string> data, IList<Mock<IFilter>> filters)
+        {
+            var pipleLine = new FilterPipeline();
+
+            foreach (var filter in filters)
+                pipleLine.AddFilter(filter.Object);
+
+
+            pipleLine.Process(data);
+
+            foreach (var filter in filters)
+                filter.Verify(x => x.Execute(It.IsAny<IEnumerable<string>>()), Times.Once);
+        }
+    }
+}
