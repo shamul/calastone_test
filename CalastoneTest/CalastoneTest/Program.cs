cba234731339575468efd71f76842c499a1ac732
shamul <shamul@evo-is.com>
2022-06-19 22:56:33 +0100

    added in console output

--- CalastoneTest/CalastoneTest/Program.cs
+++ CalastoneTest/CalastoneTest/Program.cs
@@ -1,2 +1,25 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using CalastoneTest.Filter;
+using CalastoneTest.Pipeline;
+using CalastoneTest.Repo;
+using System.IO.Abstractions;
+
+var fileRepo = new FileDataRepo(new FileSystem());
+fileRepo.Path = @"Z:\code\calastone_test\CalastoneTest\CalastoneTest\Input.txt";
+
+var cFilter = new ContainCharacterFilter('t');
+var mFilter = new MinimumLengthFilter(3);
+var vFilter = new VowelMiddleFilter();
+
+var pipeline = new FilterPipeline();
+pipeline.AddFilter(vFilter);
+pipeline.AddFilter(mFilter);
+pipeline.AddFilter(cFilter);
+
+var result = pipeline.Process(fileRepo.GetData());
+
+foreach (var item in result)
+{
+    Console.WriteLine(item);
+}
+
