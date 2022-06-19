24abd054688db7dbbca3ed174636b4bfd839c71a
shamul <shamul@evo-is.com>
2022-06-19 22:23:01 +0100

    added in file repo

---
+++ CalastoneTest/CalastoneTest/Repo/FileDataRepo.cs
@@ -0,0 +1,33 @@
+﻿using System.IO.Abstractions;
+using System.Text.RegularExpressions;
+
+namespace CalastoneTest.Repo
+{
+    public class FileDataRepo : IDataRepo
+    {
+        private readonly IFileSystem _fileSystem;
+
+        public FileDataRepo(IFileSystem fileSystem)
+        {
+            _fileSystem = fileSystem;
+        }
+
+        public string Path { get; set; }
+
+        public IEnumerable<string> GetData()
+        {
+            if(!_fileSystem.File.Exists(Path))
+                throw new FileNotFoundException(Path);
+            var rx = new Regex(@"\w+", RegexOptions.Compiled | RegexOptions.IgnoreCase);
+
+            using var streamReader = _fileSystem.File.OpenText(Path);
+            while (!streamReader.EndOfStream)
+            {
+                var line = streamReader.ReadLine();
+                var matches = rx.Matches(line);
+                foreach (var match in matches)
+                    yield return match.ToString();
+            }
+        }
+    }
+}
---
+++ CalastoneTest/CalastoneUnitTests/Repo/FileDataRepoTests.cs
@@ -0,0 +1,30 @@
+﻿using AutoFixture.NUnit3;
+using CalastoneTest.Repo;
+using FluentAssertions;
+using NUnit.Framework;
+using System.IO.Abstractions.TestingHelpers;
+
+namespace CalastoneUnitTests.Repo
+{
+    [TestFixture]
+    public class FileDataRepoTests
+    {
+        [Test]
+        [AutoData]
+        public void GetDataOutputsWords(string fileName)
+        {
+            var fileSystem = new MockFileSystem(new Dictionary<string, MockFileData> { { @$"c:\{fileName}.txt", new MockFileData("Testing is meh.") } });
+
+            var sut = new FileDataRepo(fileSystem)
+            {
+                Path = $"{fileName}.txt"
+            };
+
+            var data = sut.GetData().ToList();
+            data.Count.Should().Be(3);
+            data[0].Should().Be("Testing");
+            data[1].Should().Be("is");
+            data[2].Should().Be("meh");
+        }
+    }
+}
