621e3d86f7142d0ad02296fd73eea163951bb5d4
shamul <shamul@evo-is.com>
2022-06-19 17:46:36 +0100

    added in second filter

---
+++ CalastoneTest/CalastoneTest/Filter/ContainCharacterFilter.cs
@@ -0,0 +1,19 @@
+﻿namespace CalastoneTest.Filter
+{
+    public class ContainCharacterFilter : IFilter
+    {
+        public ContainCharacterFilter(char chracterToFilter)
+        {
+            ChracterToFilter = chracterToFilter;
+        }
+
+        public char ChracterToFilter { get; }
+
+        public IEnumerable<string> Execute(IEnumerable<string> input)
+        {
+            foreach (string inputItem in input)
+                if (!inputItem.Contains(ChracterToFilter, StringComparison.CurrentCultureIgnoreCase))
+                    yield return inputItem;
+        }
+    }
+}
---
+++ CalastoneTest/CalastoneUnitTests/Filter/ContainCharacterFilterTests.cs
@@ -0,0 +1,32 @@
+﻿using CalastoneTest.Filter;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace CalastoneUnitTests.Filter
+{
+    [TestFixture]
+    public class ContainCharacterFilterTests
+    {
+        [TestCase('t', new[] {"The", "Test", "to", "as", "I"}, 2)]
+        [TestCase('o', new[] { "The", "Test", "to", "as", "I" }, 4)]
+        [TestCase('e', new[] { "The", "Test", "to", "as", "I" }, 3)]
+        public void FilterInputCorrectly(char chracterToFilter, string[] input, int expectedLength)
+        {
+            var sut = new ContainCharacterFilter(chracterToFilter);
+
+            var result = sut.Execute(input);
+
+            result.Count().Should().Be(expectedLength);
+        }
+
+        [Test]
+        public void FilterInputIgnoreCase()
+        {
+            var sut = new ContainCharacterFilter('t');
+
+            var result = sut.Execute(new[] {"There", "there", "The", "their", "random"});
+
+            result.Count().Should().Be(1);
+        }
+    }
+}
